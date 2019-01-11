23b074d5f70fec97171ec5c9935944c34320d5f7
declanmcgee01 <declanmcgee1995@gmail.com>
2019-01-11 15:38:22 +0000

    Started the implementation of the front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import font from './norse.regular.otf'
 import './App.css';
+import {Jumbotron} from'react-bootstrap';
+import {Button} from'react-bootstrap';
+import {Navbar} from'react-bootstrap';
+import {Nav} from'react-bootstrap';
+import {NavItem} from'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <body>
+          <Navbar>
+              <Navbar.Header>
+          </Navbar.Header>
+            <Nav>
+              <NavItem eventKey={1} href="#createGod">
+                  Create God
+              </NavItem>
+              <NavItem eventKey={2} href="#deleteGod">
+                  Delete God
+              </NavItem>
+              </Nav>
+              </Navbar>
+          <Jumbotron>
+            <h1 id = "rcorners1"><div class = "third"></div>Norse Pantheon</h1>
+          </Jumbotron>
+          </body>
         </header>
       </div>
     );
